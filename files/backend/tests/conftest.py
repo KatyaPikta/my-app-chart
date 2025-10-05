69f1ae5751b59504ddf872465c130a39ff50eefe
Katya Pikta <katyapikta@gmail.com>
2025-10-05 08:38:37 +0000

    Testing with real services

--- files/backend/tests/conftest.py
+++ files/backend/tests/conftest.py
@@ -12,11 +12,24 @@ class TestConfig:
     CACHE_DEFAULT_TIMEOUT = 1  # short timeout
     FE_HOST = "localhost"  # not really used in tests
 
+class RealServicesTestConfig:
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/test_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = "RedisCache"  # use real Redis
+    CACHE_REDIS_HOST = "127.0.0.1"
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = 0
+    CACHE_DEFAULT_TIMEOUT = 1
+    FE_HOST = "127.0.0.1"
 
 @pytest.fixture
 def app():
-    app = create_app(config_class=TestConfig)
-
+    if os.getenv('USE_REAL_SERVICES'):
+        app = create_app(RealServicesTestConfig)
+    else:
+        app = create_app(TestConfig)
+        
     # Create tables before running tests
     with app.app_context():
         db.create_all()
