8776c0e197854af40a011a4892490e41d89d9051
Katya Pikta <katyapikta@gmail.com>
2025-10-15 08:05:40 +0000

    Test to bump app version

--- apps/backend/app/config.py
+++ apps/backend/app/config.py
@@ -5,13 +5,13 @@ class Config:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MySQL
+    # MySQL Test to bump App version
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
         f"@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
     )
 
-    # Redis cache
+    # Redis cache 
     CACHE_TYPE = "RedisCache"
     CACHE_REDIS_HOST = os.getenv("REDIS_HOST", "redis")
     CACHE_REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
