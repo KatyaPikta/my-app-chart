efc647f18155f188b8d56e681ff63c35e168181e
Katsiaryna Pikta <katsiaryna.pikta@modsen-software.com>
2025-12-25 23:55:18 +0300

    Test deploy backend and frontend

--- apps/backend/app/config.py
+++ apps/backend/app/config.py
@@ -5,7 +5,7 @@ class Config:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # MySQL Test to bump App version Test Test Test Test
+    # MySQL Test to bump App version Test Test Test Test test
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
         f"@{os.getenv('MYSQL_HOST')}/{os.getenv('MYSQL_DB')}"
