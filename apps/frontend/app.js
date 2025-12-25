68fa7ad0e260ad5a8471ff52bc0b0ad42be51e2f
Katsiaryna Pikta <katsiaryna.pikta@modsen-software.com>
2025-12-25 23:50:36 +0300

    Test separate frontend

--- apps/frontend/app.js
+++ apps/frontend/app.js
@@ -10,7 +10,7 @@ async function loadTasks() {
     const el = document.createElement("div");
     el.innerHTML = `<span>${t.title}: ${t.description} — ${t.status}</span>`;
 
-    // Create button container test - test autodeploy separate
+    // Create button container test - test autodeploy separate Test
     const btnContainer = document.createElement("div");
     btnContainer.style.marginLeft = "auto";
     btnContainer.style.display = "flex";
